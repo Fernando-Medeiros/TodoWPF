ea5c7ece1dc125c8ead7f8565660370f4a9b3075
Fernando-Medeiros <fernandomedeiros3k@gmail.com>
2024-06-01 20:35:36 -0300

    style: Resource > Task Icon

--- Resource/Icon.cs
+++ Resource/Icon.cs
@@ -5,5 +5,7 @@
         public const string Login = "../Resource/Images/login.png";
         public const string Email = "../Resource/Images/email.png";
         public const string Password = "../Resource/Images/password.png";
+        public const string Task = "../Resource/Images/task.png";
+        public const string User = "../Resource/Images/user.png";
     }
 }
