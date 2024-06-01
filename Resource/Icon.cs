9e834bb17858d8d3ec48ddb91d7e7df2f56144b4
Fernando-Medeiros <fernandomedeiros3k@gmail.com>
2024-05-31 21:48:34 -0300

    style: Resource > Images

---
+++ Resource/Icon.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoWPF.Resources
+{
+    public static class Icon
+    {
+        public const string Login = "../Resource/Images/login.png";
+        public const string Email = "../Resource/Images/email.png";
+        public const string Password = "../Resource/Images/password.png";
+
+    }
+}
