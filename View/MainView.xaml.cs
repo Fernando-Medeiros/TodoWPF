07d957680062e9a0ba61003c8bcc4d26125202aa
Fernando-Medeiros <fernandomedeiros3k@gmail.com>
2024-05-31 21:55:45 -0300

    feat: View > MainView

---
+++ View/MainView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows.Controls;
+using TodoWPF.ViewModel;
+
+namespace TodoWPF.View
+{
+    public partial class MainView : Page
+    {
+        public MainView()
+        {
+            InitializeComponent();
+            DataContext = App.Current.Services.GetService<MainViewModel>();
+        }
+    }
+}
