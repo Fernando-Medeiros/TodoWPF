79419e08c776b47c3c5bf207be36c543535475ec
Fernando-Medeiros <fernandomedeiros3k@gmail.com>
2024-06-01 11:51:54 -0300

    feat: View > NewAccountView

---
+++ View/NewAccountView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows.Controls;
+using TodoWPF.ViewModel;
+
+namespace TodoWPF.View
+{
+    public sealed partial class NewAccountView : Page
+    {
+        public NewAccountView()
+        {
+            InitializeComponent();
+            DataContext = App.Current.Services.GetService<MainViewModel>();
+        }
+    }
+}
