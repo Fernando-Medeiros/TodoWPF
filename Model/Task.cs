7109d4aff8112e6b65c75a1582001fe1ef6ebbf0
Fernando-Medeiros <fernandomedeiros3k@gmail.com>
2024-06-01 13:21:22 -0300

    feat: Model Task > add CustomerId [FK]

--- Model/Task.cs
+++ Model/Task.cs
@@ -6,7 +6,7 @@ namespace TodoWPF.Model
     {
         public Guid Id { get; set; }
         public string Title { get; set; }
-        public string Description { get; set; }
         public bool IsCompleted { get; set; }
+        public Guid CustomerId { get; set; }
     }
 }
