a4fbe6acdf6e1d8d6d4da19ad209cfad22128892
Fernando-Medeiros <fernandomedeiros3k@gmail.com>
2024-05-31 21:50:30 -0300

    feat: Service > BaseViewModel : INotifyPropertyChanged

---
+++ Service/BaseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace TodoWPF.Service
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        private bool isBusy;
+
+        protected bool IsBusy()
+        {
+            if (isBusy) return true;
+            isBusy = true;
+            return false;
+        }
+
+        protected void NotBusy() => isBusy = false;
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName = null) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
