ce72c701409f6f406907a2773d33172f3619407d
Fernando-Medeiros <fernandomedeiros3k@gmail.com>
2024-05-31 21:54:52 -0300

    feat: App > IOC [ServiceProvider]

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,40 @@
-﻿using System.Windows;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Windows;
+using TodoWPF.ViewModel;
 
 namespace TodoWPF
 {
-    /// <summary>
-    /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Services = ConfigureServices();
+
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Gets the current <see cref="App"/> instance in use
+        /// </summary>
+        public new static App Current => (App)Application.Current;
+
+        /// <summary>
+        /// Gets the <see cref="IServiceProvider"/> instance to resolve application services.
+        /// </summary>
+        /// 
+        public IServiceProvider Services { get; }
+
+        /// <summary>
+        /// Configures the services for the application.
+        /// </summary>
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddSingleton<MainViewModel>();
+
+            return services.BuildServiceProvider();
+        }
     }
 }
