890726731d8687cf5c6ac2571b068b8484f2ab54
Fernando-Medeiros <fernandomedeiros3k@gmail.com>
2024-06-01 11:52:13 -0300

    feat: View > RecoverPasswordView

---
+++ View/RecoverPasswordView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows.Controls;
+using TodoWPF.ViewModel;
+
+namespace TodoWPF.View
+{
+    public sealed partial class RecoverPasswordView : Page
+    {
+        public RecoverPasswordView()
+        {
+            InitializeComponent();
+            DataContext = App.Current.Services.GetService<MainViewModel>();
+        }
+    }
+}
