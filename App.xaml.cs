245a490ad533a2afd7b3831bba2cbec3d2b1e2a8
Fernando-Medeiros <fernandomedeiros3k@gmail.com>
2024-06-01 11:50:05 -0300

    feat: App > GoTo (Navigation)

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Windows;
+using TodoWPF.Resource;
+using TodoWPF.View;
 using TodoWPF.ViewModel;
 
 namespace TodoWPF
 {
-    public partial class App : Application
+    public sealed partial class App : Application
     {
         public App()
         {
@@ -25,16 +27,38 @@ namespace TodoWPF
         /// 
         public IServiceProvider Services { get; }
 
+        private MainWindow ThisMainWindow => (MainWindow)Application.Current.MainWindow;
+
         /// <summary>
         /// Configures the services for the application.
         /// </summary>
         private static IServiceProvider ConfigureServices()
         {
-            var services = new ServiceCollection();
+            return new ServiceCollection()
+                .AddTransient<MainView>()
+                .AddTransient<NewAccountView>()
+                .AddTransient<RecoverPasswordView>()
+                .AddSingleton<MainViewModel>()
+                .BuildServiceProvider();
+        }
 
-            services.AddSingleton<MainViewModel>();
-
-            return services.BuildServiceProvider();
+        /// <summary>
+        /// Configures the navigation for the application.
+        /// </summary>
+        public void GoTo(Endpoint endpoint)
+        {
+            switch (endpoint)
+            {
+                case Endpoint.Login:
+                    ThisMainWindow.Main.Content = Services.GetService<MainView>();
+                    break;
+                case Endpoint.SignUp:
+                    ThisMainWindow.Main.Content = Services.GetService<NewAccountView>();
+                    break;
+                case Endpoint.Recover:
+                    ThisMainWindow.Main.Content = Services.GetService<RecoverPasswordView>();
+                    break;
+            }
         }
     }
 }
