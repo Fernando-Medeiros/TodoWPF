aa3ede5e29ad7f5d734aec33404fe9d258b8e849
Fernando-Medeiros <fernandomedeiros3k@gmail.com>
2024-05-31 21:49:02 -0300

    feat: Model > [Customer, Task](Base)

---
+++ Model/Task.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TodoWPF.Model
+{
+    public sealed class Task
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
